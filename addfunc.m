78f4b2e9309994fec6e9f6efdcffe7e896113642
SumanthReddyA <asreddy89@outlook.com>
2018-04-22 17:24:40 +0530

    support for multiple input arguments

--- addfunc.m
+++ addfunc.m
@@ -1,9 +1,10 @@
-function c = addfunc(a,b)
-if isnumeric(a) && isnumeric(b)
-c=a+b;
+function c = addfunc(varargin)
+f1 = @(x) isnumeric(x);
+t=cellfun(f1,varargin);
+if(sum(t)== nargin)
+    c= sum(cell2mat(varargin));
 else
     c=NaN;
-disp("Enter only numuric values");   
+    disp("Enter only numuric values");
 end
 end
-
